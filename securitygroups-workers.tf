30ba2ae88321011093ed30e5d872b6090df3127d
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2020-07-20 12:24:09 +0530

    Update securitygroups-workers.tf

--- securitygroups-workers.tf
+++ securitygroups-workers.tf
@@ -33,7 +33,7 @@ resource "aws_security_group_rule" "demo-node-ingress-cluster" {
   to_port = 35000
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "aws_security_group.demo-node.id"
+  security_group_id = "sg-0611c2f611c143fcd"
 }
 
 
