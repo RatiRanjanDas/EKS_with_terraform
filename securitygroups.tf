4dd7f82661fd767f1c59d63733b5d047095d0fb7
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2020-07-20 15:45:35 +0530

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -3,6 +3,18 @@ resource "aws_security_group" "demo-cluster" {
   description = "Cluster communication with worker nodes"
   vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 30000
+    to_port     = 35000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -13,41 +25,4 @@ resource "aws_security_group" "demo-cluster" {
   tags = {
     Name = "terraform-eks-demo"
   }
-}
-
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
-  description              = "Allow pods to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.demo-cluster.id
-  source_security_group_id = aws_security_group.demo-node.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "demo-cluster-ingress-node-tcp" {
-  type = "ingress"
-  from_port = 30000
-  to_port = 35000
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-001"
-}
-  
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  cidr_blocks       = [local.workstation-external-cidr]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.demo-cluster.id
-  to_port           = 443
-  type              = "ingress"
 }
