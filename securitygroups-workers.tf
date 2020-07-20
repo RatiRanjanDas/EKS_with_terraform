c07b85ae9d3f27a087151e27f5f94dc22c6ebb1d
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2020-07-20 15:49:08 +0530

    Update securitygroups-workers.tf

--- securitygroups-workers.tf
+++ securitygroups-workers.tf
@@ -4,36 +4,29 @@ resource "aws_security_group" "demo-node" {
   description = "Security group for all nodes in the cluster"
   vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 30000
+    to_port     = 35000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+    
   tags = {
     "Name"                                      = "terraform-eks-demo-node"
     "kubernetes.io/cluster/${var.cluster-name}" = "owned"
   }
-}
-
-resource "aws_security_group_rule" "demo-node-ingress-self" {
-  description              = "Allow node to communicate with each other"
-  from_port                = 443
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.demo-node.id
-  source_security_group_id = aws_security_group.demo-node.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "demo-node-ingress-cluster" {
-  type = "ingress"
-  from_port = 30000
-  to_port = 35000
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-002"
 }
 
 
