2f0b0aa9c4e88ecfb119b44f3549570e36ba98f9
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2020-07-20 12:32:24 +0530

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -31,7 +31,7 @@ resource "aws_security_group_rule" "demo-cluster-ingress-node-tcp" {
   to_port = 35000
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-0611c2f611c143fcd"
+  security_group_id = "sg-001"
 }
   
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
